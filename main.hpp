cac6bdc03c60d676a145c2590bfbb392b503cc9c
Martin5587 <mcontreras265@insite.4cd.edu>
2024-03-15 16:24:24 -0700

    commit(1)

--- main.hpp
+++ main.hpp
@@ -1,8 +1,26 @@
+#include <iostream>
+#include <cmath>
 
-//********************
-// Declare Function Prototypes
-//********************
+using namespace std;
 
-// ******************************
-// Implement all your functions here
-// ******************************
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (int i = 5; i * i <= n; i += 6) {
+        if ( n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+    return true;
+}
+
+void getTwoValues( int &begin, int &end) {
+    do {
+        cout << "Enter 2 integer values: ";
+        cin >> begin >> end;
+    } while (begin >= end);
+}
+
+
